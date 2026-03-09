eb3300040ba2dab3189dec4a4f46fceef388767f
12aksahth <sanubha2006@gmail.com>
2026-03-09 14:49:45 +0530

    [garvkhandelwal1210] Added [feature/UC1-WelcomePage]

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,34 @@
+/**
+ * HotelBookingApplication
+ *
+ * This class represents the entry point of the Hotel Booking System.
+ * It demonstrates how a Java application begins execution and prints
+ * a welcome message along with application name and version.
+ *
+ * The JVM starts program execution from the main() method.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+
+public class BookMyStayApp{
+
+    /**
+     * Main method - Entry point of the application.
+     * JVM calls this method when the program starts.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("Welcome to the Hotel Booking System");
+
+        // Application details
+        System.out.println("Application Name : Hotel Booking System");
+        System.out.println("Version          : v1.0");
+
+        // Application end message
+        System.out.println("Application started successfully.");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# BookMyStayApp
-APP
+                                                                                                                                                                                                                                                   
